2085da0d22ae7469554fc410db91fde96d546eba
grt <saifulgrtanwar@gmail.com>
2024-11-02 15:33:12 +0700

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+import { socket } from "$lib/socket";
+import "../app.css";
+import { onDestroy, onMount } from "svelte";
+
+function onMessage(msg: string) {
+    console.log(`Message : "${msg}"`)
+}
+
+onMount(() => {
+    socket.on('message', onMessage)
+})
+
+onDestroy(() => {
+    socket.off('message', onMessage)
+})
+
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+import { io } from "socket.io-client"
+import { onDestroy, onMount } from "svelte";
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
